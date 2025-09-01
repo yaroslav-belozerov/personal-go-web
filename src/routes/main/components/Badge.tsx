ef5ab9b044eef9c28f5a1cc4aac6cbc2263d04b0
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-09-01 21:11:16 +0200

    badge fixxxx

--- src/routes/main/components/Badge.tsx
+++ src/routes/main/components/Badge.tsx
@@ -3,17 +3,19 @@ export default function Badge({ src, link }: { src: string; link?: string }) {
     return (
       <img
         src={src}
+        style="image-rendering: pixelated;"
         alt=""
-        class="w-[88px] h-[31px] md:hover:scale-140 transition-all"
+        class="w-[88px] h-[31px] md:hover:scale-180 transition-all duration-200"
       />
     );
   } else {
     return (
       <a href={link} target="_blank" rel="noopener noreferrer">
         <img
+          style="image-rendering: pixelated;"
           src={src}
           alt=""
-          class="w-[88px] h-[31px] md:hover:scale-140 transition-all"
+          class="w-[88px] h-[31px] md:hover:scale-180 transition-all duration-200"
         />
       </a>
     );
